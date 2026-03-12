c99ce4ef83766905289079a29617bd245f0f5400
SnehaY2004 <aashisupreme20@gmail.com>
2026-03-12 10:48:21 +0530

    fix: enable CORS with specific origin for better security

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use('/books', bookRoutes);
